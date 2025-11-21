abfb7dd65ce9332ab66c997754de4c7c90a9228b
carlosfc02 <carlosfalconcastellano@gmail.com>
2025-11-21 09:20:04 +0000

    Edited

--- train.py
+++ train.py
@@ -4,17 +4,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
 
 # --- CONFIGURACIÓN ---
-img_height, img_width = 48, 48  # Tamaño estándar de FER-2013
+img_height, img_width = 48, 48 
 batch_size = 64
-epochs = 15  # Con 15 o 20 suele bastar para aprobar
+epochs = 15  
 num_classes = 7 # angry, disgust, fear, happy, neutral, sad, surprise
 
-# Rutas (AJUSTA ESTO si tu carpeta se llama diferente)
+# Rutas 
 train_dir = 'images/train'
 val_dir = 'images/validation'
 
 # --- 1. GENERADORES DE DATOS (Data Augmentation) ---
-# Esto crea variaciones de las fotos para que el modelo aprenda mejor
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     rotation_range=10,
@@ -28,7 +27,7 @@ train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    color_mode="grayscale", # Importante: FER-2013 es en blanco y negro
+    color_mode="grayscale", 
     class_mode='categorical'
 )
 
@@ -60,7 +59,7 @@ model = Sequential([
     MaxPooling2D(pool_size=(2, 2)),
     Dropout(0.25),
 
-    # Capas Densas (Clasificación)
+    # Clasificación
     Flatten(),
     Dense(128, activation='relu'),
     BatchNormalization(),
